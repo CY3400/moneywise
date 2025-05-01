c68f600119cf6d87db1e147ac6328a08eca65085
Charbel <charbelymn@gmail.com>
2025-05-01 13:52:16 +0300

    Delete main directory

--- main/java/com/charbel/finance_app/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.charbel.finance_app.repository;
-
-import com.charbel.finance_app.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    
-}
--- main/java/com/charbel/finance_app/repository/DescriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.charbel.finance_app.repository;
-import com.charbel.finance_app.model.Description;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DescriptionRepository extends JpaRepository<Description, Long> {
-    
-}

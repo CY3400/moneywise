efd37e04286f5c7966d946bd97fec114da2cc658
Charbel Yammine <charbelymn@gmail.com>
2025-05-05 15:31:48 +0300

    Correcton de bug sur Description

--- src/main/resources/static/JS/description.js
+++ src/main/resources/static/JS/description.js
@@ -199,7 +199,7 @@ document.addEventListener("DOMContentLoaded", function(){
     description.addEventListener("keyup", function(){
         const selectedType = document.querySelector('input[name="types"]:checked');
 
-        loadDescriptions(description.value,selectedType.value);
+        loadDescriptions(description.value,selectedType);
     });
 
     document.querySelectorAll('input[name="types"]').forEach(radio => {

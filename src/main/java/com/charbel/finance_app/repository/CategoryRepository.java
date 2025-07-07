b740706a06572ce64edf2860d9c02cb9aa100bc9
Charbel Yammine <charbelymn@gmail.com>
2025-07-07 15:36:07 +0300

    Ajout de 2 nouvelles requetes: une pour les categories actives et une pour filtrer les categories

--- src/main/java/com/charbel/finance_app/repository/CategoryRepository.java
+++ src/main/java/com/charbel/finance_app/repository/CategoryRepository.java
@@ -1,8 +1,17 @@
 package com.charbel.finance_app.repository;
 
 import com.charbel.finance_app.model.Category;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    
+    @Query(value="SELECT * FROM CATEGORY WHERE STATUS = 2 ORDER BY DESCRIPTION",nativeQuery = true)
+    List<Category> findAllActives();
+
+    @Query(value="SELECT * FROM CATEGORY WHERE (LOWER(DESCRIPTION) LIKE CONCAT('%', LOWER(:description), '%') OR :description IS NULL) ORDER BY DESCRIPTION",nativeQuery = true)
+    List<Category> findCategoryByName(@Param("description") String description);
 }
